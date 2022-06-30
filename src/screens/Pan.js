5fc03eeaf18a8aef9a9b5a80806ec9f6c44f197d
Biswanath Tewari <biswanath@geekyants.com>
2022-06-30 21:29:39 +0530

    feat: ✨ springed center

--- src/screens/Pan.js
+++ src/screens/Pan.js
@@ -1,8 +1,9 @@
 import React, {useRef} from 'react';
-import {StyleSheet, PanResponder, Animated} from 'react-native';
+import {StyleSheet, PanResponder, Animated, Text} from 'react-native';
 
 const Pan = () => {
   const pan = useRef(new Animated.ValueXY()).current;
+  const centerPan = useRef(new Animated.ValueXY()).current;
 
   const panResponder = useRef(
     PanResponder.create({
@@ -37,23 +38,86 @@ const Pan = () => {
       },
     }),
   ).current;
+
+  const centerRensponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true, // should we start panning?
+      onPanResponderGrant: () => {
+        centerPan.setOffset({
+          x: centerPan.x._value,
+          y: centerPan.y._value,
+        });
+        //centerPan.setValue({x: 0, y: 0});
+      },
+      onPanResponderMove: (_, gestures) => {
+        centerPan.setValue({x: gestures.dx, y: gestures.dy});
+      },
+      onPanResponderRelease: () => {
+        Animated.spring(centerPan, {
+          toValue: {x: 0, y: 0}, // we can create tinder like card by adjusting the spring values
+          useNativeDriver: true,
+        }).start();
+      },
+    }),
+  ).current;
+
+  const rotate = centerPan.x.interpolate({
+    inputRange: [-200, 0, 200],
+    outputRange: ['-30deg', '0deg', '30deg'],
+  });
+
+  const opacity = centerPan.x.interpolate({
+    inputRange: [-200, 0, 200],
+    outputRange: [0.5, 1, 0.5],
+  });
   return (
-    <Animated.View
-      style={[
-        style.ball,
-        {transform: [{translateX: pan.x}, {translateY: pan.y}]},
-      ]}
-      {...panResponder.panHandlers} //destructuring panResponder's evennt Handlers
-    ></Animated.View>
+    <>
+      <Animated.View
+        style={[
+          style.ball,
+          {transform: [{translateX: pan.x}, {translateY: pan.y}]},
+        ]}
+        {...panResponder.panHandlers} //destructuring panResponder's evennt Handlers
+      >
+        <Text>I can move freely</Text>
+      </Animated.View>
+      <Animated.View
+        style={[
+          style.box,
+          {
+            transform: [
+              {translateX: centerPan.x},
+              {
+                translateY: centerPan.y,
+              },
+              {rotate: rotate},
+            ],
+            opacity: opacity,
+          },
+        ]}
+        {...centerRensponder.panHandlers}>
+        <Text>Fixed to center</Text>
+      </Animated.View>
+    </>
   );
 };
 
 const style = StyleSheet.create({
   ball: {
-    width: 200,
-    height: 200,
+    width: 100,
+    height: 100,
     backgroundColor: 'red',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  box: {
+    width: 125,
+    height: 125,
+    backgroundColor: 'blue',
     borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
