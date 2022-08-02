16071ae6a6ab0bec007949da686e786e6897ffec
Biswanath Tewari <biswanath@geekyants.com>
2022-08-02 15:19:07 +0530

    feat: ✨ rotation

--- src/screens/index.js
+++ src/screens/index.js
@@ -7,3 +7,4 @@ export * from './Animations/Scale';
 export * from './Animations/HeightWidth';
 export * from './Animations/Positioned';
 export * from './Animations/Bgcolor';
+export * from './Animations/Rotation';
